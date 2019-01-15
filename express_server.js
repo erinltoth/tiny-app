40e5aaadcbe17e45ad7e915d48ee68c76b12916f
Erin Toth <erintoth@gmail.com>
2019-01-15 22:23:36 +0000

    First commit of tinyApp project with one stream in Express

---
+++ express_server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; //setting default port 8080
+
+app.set("view engine", "ejs");
+
+const urlDatabase = [
+  { shortURL: "b2xVn2", fullURL: "http://www.lighthouselabs.ca"},
+  { shortURL: "9sm5xK", fullURL: "http://www.google.com"}
+];
+
+app.get("/", (request, response) => {
+  response.send("Hello!");
+});
+
+app.get("/urls.json", (request, response) => {
+  response.json(urlDatabase);
+});
+
+app.get("/hello", (request, response) => {
+  response.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (request, response) => {
+  let templateVars = { urls: urlDatabase };
+  response.render("urls_index", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
