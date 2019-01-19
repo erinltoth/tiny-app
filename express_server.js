cb239a19bd22e009bc36514063e1de6c61f20f82
Erin Toth <erintoth@gmail.com>
2019-01-19 21:53:33 +0000

    Completely refactored to change urlDatabase back to an object rather than an array and functions rather than for loops within endpoints to remove errors with calling responses within for loops

--- express_server.js
+++ express_server.js
@@ -4,21 +4,35 @@ const PORT = 8080; //setting default port 8080
 const bodyParser = require("body-parser");
 // const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
-var cookieSession = require('cookie-session')
+
+const cookieSession = require('cookie-session')
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+}));
 
 app.set("view engine", "ejs");
-app.use(cookieParser());
+// app.use(cookieParser());
 
-const urlDatabase = [
-  { shortURL: "b2xVn2", fullURL: "http://www.lighthouselabs.ca", userID: "59284"},
-  { shortURL: "9sm5xK", fullURL: "http://www.google.com", userID: "01938"}
-];
+const urlDatabase = {
+  "b2xVn2": {
+    shortURL: "b2xVn2",
+    fullURL: "http://www.lighthouselabs.ca",
+    userID: "59284"
+  },
+  "9sm5xK": {
+    shortURL: "9sm5xK",
+    fullURL: "http://www.google.com",
+    userID: "01938"
+  }
+};
+
 
 const users = {
   "59284" : {
     id: "59284",
-    email: "erintoth@gmail.com",
-    password: "sammich"
+    email: "cat@cat.com",
+    password: "cat"
   },
   "01938" : {
     id: "01938",
@@ -27,180 +41,183 @@ const users = {
   }
 };
 
-var errors = {
-  '400': (request, response) => {
-    response.statusCode = 400;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Not Found\n');
-  },
-  '403': (request, response) => {
-    response.statusCode = 403;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Forbidden\n');
-  }
-};
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (request, response) => {
-  response.send("Hello!");
-});
-
-app.get("/urls.json", (request, response) => {
-  response.json(urlDatabase);
-});
-
-app.get("/hello", (request, response) => {
-  response.send("<html><body>Hello <b>World</b></body></html>\n");
+  if (request.session.user_id) {
+    response.redirect("/urls");
+  } else {
+    response.redirect("/login");
+  }
 });
 
 app.get("/urls", (request, response) => {
-  let templateVars = { user: ''};
-  let user = fetchUser(request.session["user_id"]);
-  if (user != undefined) {
-    let userId = user.id;
-    let userUrlDatabase = urlsForUser(userId);
-    let templateVars = { urls: userUrlDatabase, user: fetchUser(request.session["user_id"])};
-    return templateVars;
+  if (request.session.user_id){
+    let templateVars = {
+      user: fetchUser(request.session.user_id),
+      urls: urlsForUser(request.session.user_id)
+    };
+    response.render("urls_index", templateVars);
+  } else {
+    response.send("You must login to see your URLs! <a href=/login>Login</a> to see your links!");
   }
-  response.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (request, response) => {
-  let templateVars = { urls: urlDatabase, user: fetchUser(request.session["user_id"]) };
-  let user = fetchUser(request.session["user_id"]);
-  if (user != undefined) {
-    let userId = user.id;
-    if (userId) {
-      response.render("urls_new", templateVars);
+  if (request.session.user_id){
+    let templateVars = {
+      urls: urlsForUser(request.session.user_id),
+      user: fetchUser(request.session.user_id)
+    };
+    response.render("urls_new", templateVars);
+  } else {
+    response.redirect('/login');
+  }
+});
+
+
+app.get("/urls/:id", (request, response) => {
+  if (request.session.user_id) {
+      if (checkShortUrl(request.params.id)) {
+        if (checkOwner(request.session.user_id)) {
+          let templateVars = {
+          urls: urlByShort(request.params.id),
+          user: fetchUser(request.session.user_id)
+        };
+        response.render("urls_show", templateVars);
+        } else {
+          response.send("You do not own this URL. Please go <a href=/urls/new>make your own</a>");
+        }
+
+      } else {
+        console.log(checkShortUrl(request.params.id));
+        console.log(checkOwner(request.session.user_id));
+        console.log(urlByShort(request.params.id));
+        response.send("This is not a valid shortURL! Please go <a href=/urls/new>make one</a>");
+      }
+  } else {
+    response.status(403).send("Please <a href=/login>login</a> to access this page");
+  }
+});
+
+app.get('/u/:shortURL', (request, response) => {
+  if (checkShortUrl(request.params.shortURL)) {
+    response.redirect(fetchLongUrl(request.params.shortURL));
+  } else {
+    response.send("I'm sorry, that isn't a valid redirection link. Please go <a href=/urls/new>make one</a>");
+  }
+});
+
+app.post("/urls", (request, response) => {
+  if (request.session.user_id) {
+    let newShort = generateRandomString (1, 62);
+    urlDatabase[newShort] = {
+      shortURL: newShort,
+      fullURL: request.body.longURL,
+      userID: request.session.user_id
+    }
+    let redirectLink = `/urls/${newShort}`;
+    response.redirect(redirectLink);
+    } else {
+      response.status(403).send("Please <a href=/login>login</a> to access this page");
+    }
+});
+
+app.post("/urls/:id", (request, response) => {
+  if (request.session.user_id) {
+      if (checkShortUrl(request.params.id)) {
+        if (checkOwnerLong(request.session.user_id, request.params.id)) {
+          urlDatabase[request.params.id].fullURL = request.body.newLongURL;
+          response.redirect("/urls");
+        } else {
+          reponse.send("You do not own this URL. Please go <a href=/urls/new>make your own</a>");
+        }
     }
   } else {
-  response.redirect('/login');
+    response.status(403).send("Please <a href=/login>login</a> to access this page");
+  }
+});
+
+app.post("/urls/:id/delete", (request, response) => {
+  if (request.session.user_id) {
+    if (checkOwnerLong(request.session.user_id, request.params.id)){
+      delete urlDatabase[request.params.id];
+      response.redirect("/urls");
+    } else {
+        response.send("You do not own this URL. Please go <a href=/urls/new>make your own</a>");
+
+    }
+    } else {
+    response.status(403).send("Please <a href=/login>login</a> to access this page");
+  }
+});
+
+app.get("/login", (request, response) => {
+  if (request.session.user_id) {
+    response.redirect("/urls");
+  } else {
+    let templateVars = {
+      user : fetchUser(request.session.user_id)
+    };
+    response.render("user_login", templateVars);
   }
 });
 
 app.get("/register", (request, response) => {
-  let templateVars = { urls: urlDatabase, user: fetchUser(request.session["user_id"]) };
-  response.render("user_reg", templateVars);
-});
-
-app.get("/login", (request, response) => {
-  let templateVars = { urls: urlDatabase, user: fetchUser(request.session["user_id"]) };
-  response.render("user_login", templateVars);
-});
-
-app.post("/register", (request, response) => {
-  let newUserEmail = request.body.email;
-  let newUserPass = request.body.password;
-  const hashedPass = bcrypt.hashSync(newUserPass, 10);
-  if (newUserEmail && newUserPass) {
-    for (let UserId in users) {
-      if (users[UserId].email === newUserEmail) {
-      errors['400'](request, response);
-      } else {
-      let newUserId = (generateRandomString (1, 62)).toString();
-      users[newUserId] = { id: newUserId, email: newUserEmail, password: hashedPass };
-      request.session.user_id = "newUserId";
-      // response.redirect('/urls');
-      }
-    }
+  if (request.session.user_id) {
+    let templateVars = {
+      urls: urlsForUser(request.session.user_id),
+      user: fetchUser(request.session.user_id)
+    };
+    response.redirect("/urls");
   } else {
-      errors['400'](request,response);
+    let templateVars = {
+      user : fetchUser(request.session.user_id)
+    };
+    response.render("user_reg", templateVars);
   }
-  console.log(users);
-  response.redirect('/login');
-});
-
-app.post("/urls", (request, response) => {
-  let newShort = generateRandomString (1, 62);
-  let newLong = Object.values(request.body);
-  let user = fetchUser(request.session["user_id"]);
-  let userId = user.id;
-  let newRequest = {shortURL: newShort, fullURL: newLong.join(), userID: userId };
-  urlDatabase.push(newRequest);
-  // console.log(urlDatabase);
-  let redirectLink = `/urls/${newShort}`;
-  // console.log(redirectLink);
-  console.log(user);
-  response.redirect(redirectLink);
-});
-
-app.post("/urls/:id/delete", (request, response) => {
-  const id = request.params.id;
-  const index = urlDatabase.indexOf(id);
-  let user = fetchUser(request.session["user_id"]);
-  let userId = user.id;
-  for (let item of urlDatabase) {
-    if (item.userID === userId && item.shortURL === id) {
-      urlDatabase.splice(index - 1, 1);
-    }
-  }
-  response.redirect('/urls');
 });
 
 app.post("/login", (request, response) => {
-  let password = request.body.password;
-  let hashedPassword = bcrypt.hashSync(password, 10);
-  let email = request.body.email;
-  // const userId = users.;
-  for (let userId in users) {
-    if (users[userId].email === email && bcrypt.compareSync(users[userId].password, hashedPassword)) {
-      request.session.user_id = "userId";
-    } else {
-      errors['403'](request,response);
+  if (checkPassword(request.body.email, request.body.password)) {
+    request.session.user_id = fetchUserIdByEmail(request.body.email);
+    response.redirect("/urls");
+  } else {
+      response.status(403).send('Please <a href=/login>try again</a> - your password or e-mail are not correct');
     }
+});
 
-    console.log(email);
-    console.log(password);
+
+
+
+
+app.post("/register", (request, response) => {
+  if(!request.body.email || !request.body.password){
+    response.send("Please <a href=/register>try again</a> and enter an email and password");
+  } else {
+      if(checkEmailTaken(request.body.email)){
+        response.send("Sorry! Email already taken. Please <a href=/login>try again</a>");
+      } else {
+        request.session.user_id = generateRandomString(1, 62);
+        users[request.session.user_id] = {
+          id: request.session.user_id,
+          email: request.body.email,
+          password: bcrypt.hashSync(request.body.password, 10)
+        }
+        response.redirect('/urls');
+
+      }
   }
-  // console.log(request.cookies["user_id"]);
-  response.redirect("/");
 });
 
 app.post("/logout", (request, response) => {
-  response.clearCookie('user_id');
+  request.session = null;
   response.redirect("/urls");
 });
 
-app.get("/urls/:id", (request, response) => {
-  const userCID = request.session['user_id'];
-  let templateVars = { shortURL: request.params.id, fullURL: urlDatabase, user: fetchUser(request.session["user_id"])};
-  response.render("urls_show", templateVars);
-});
-
-app.post("/urls/:id", (request, response) => {
-  const id = request.params.id;
-  const updatedURL = request.body.newLongURL;
-  let user = fetchUser(request.session["user_id"]);
-  let userId = user.id;
-  for (let item of urlDatabase) {
-    if (item.shortURL == id && item.userID === userId) {
-      item.fullURL = updatedURL;
-    }
-  }
-  response.redirect(`/urls/${id}`);
-});
-
-app.get('/u/:shortURL', (request, response) => {
-  const newShortPath = request.originalUrl;
-  const newShortString = newShortPath.split('').slice(3).join('');
-  const actualSite = [];
-  for (let item of urlDatabase){
-    if (item.shortURL == newShortString){
-      actualSite.push(item.fullURL);
-    }
-  }
-  response.redirect(actualSite);
-
-});
-
-app.get('/notvalid', (response, request) => {
-  response.send('Hey! That is not a valid shorURL!');
-});
-
-
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Erin's TinyApp is now listening on port ${PORT}!`);
 });
 
 function fetchUser(user_id) {
@@ -208,15 +225,97 @@ function fetchUser(user_id) {
   return userObject;
 };
 
-function urlsForUser(id) {
- let userUrlDatabase = [];
- for (let item of urlDatabase) {
-  if (item.userID === id) {
-    userUrlDatabase.push(item);
+function fetchUserIdByEmail(email) {
+  let user_id = "";
+  for (let user in users) {
+    if (users[user].email === email) {
+      user_id = users[user].id;
+    }
   }
- }
- return userUrlDatabase;
+  return user_id;
+}
+
+function urlByShort(shortURL) {
+  let urlsObj = {};
+  for (let item in urlDatabase) {
+    if (urlDatabase[item].shortURL === shortURL) {
+      urlsObj = urlDatabase[item];
+    }
+  }
+  return urlsObj;
+}
+
+function urlsForUser(user_id) {
+  let urlsArray = [];
+  for (let item in urlDatabase) {
+    if (user_id === urlDatabase[item].userID) {
+      urlsArray.push(urlDatabase[item]);
+    }
+  }
+ return urlsArray;
 };
+
+function checkEmailTaken(email){
+  let flag = false;
+  for(var user in users){
+    if(users[user].email === email){
+      flag = true;
+    }
+  }
+  return flag;
+};
+
+function checkShortUrl(shortURL) {
+  let flag = false;
+  for(var url in urlDatabase){
+    if(urlDatabase[url].shortURL === shortURL){
+      flag = true;
+    }
+  }
+  return flag;
+};
+
+function fetchLongUrl(shortURL) {
+  let longURL = "";
+  for (var url in urlDatabase) {
+    if (urlDatabase[url].shortURL === shortURL) {
+      longURL = urlDatabase[url].fullURL;
+    }
+  }
+  return longURL;
+}
+
+function checkOwner(user_id) {
+  let flag = false;
+  for(let url in urlDatabase) {
+    if(urlDatabase[url].userID === user_id) {
+      flag = true;
+    }
+  }
+  return flag;
+};
+
+function checkOwnerLong(user_id, shortURL) {
+  let flag = false;
+  for (let url in urlDatabase) {
+    if (urlDatabase[url].userID === user_id && urlDatabase[url].shortURL === shortURL) {
+      flag = true;
+    }
+  }
+  return flag;
+}
+
+function checkPassword(email, password) {
+  let flag = false;
+  for (let user in users) {
+    if (users[user].email === email) {
+      if (bcrypt.compareSync(password, users[user].password)) {
+        flag = true;
+      }
+    }
+  }
+  return flag;
+}
 
 function generateRandomString(min, max) {
   const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
